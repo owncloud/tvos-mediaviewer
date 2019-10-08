cce618bef6be32c957e9cdba100360b02b08e94d
Matthias Hühne <github@hosy.de>
2019-10-08 20:53:43 +0200

    Initial Commit

---
+++ ownCloud Media/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ownCloud Media
+//
+//  Created by Matthias Hühne on 08.10.19.
+//  Copyright © 2019 Matthias Hühne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    HStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    HStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
